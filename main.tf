2c658f1f34d2c3b10f71b7975716b70a4f2a6763
Vladlen Fedosov <vlad.fedosov@gmail.com>
2018-03-17 12:33:32 +0200

    ghfh

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
 resource "null_resource" "cluster_new1" {
   # Changes to any instance of the cluster requires re-provisioning
   triggers = {
-    "tst"
+    zzz = "tst"
   }
 
 }
