657311e2f88e27e9d8c5ce852de932d7415bb58c
Vladlen Fedosov <vlad.fedosov@gmail.com>
2018-03-17 21:26:12 +0200

    tst

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
 
-# Configure the AWS Provider
-provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "us-west-1"
-}
+# # Configure the AWS Provider
+# provider "aws" {
+#   access_key = "${var.aws_access_key}"
+#   secret_key = "${var.aws_secret_key}"
+#   region     = "us-west-1"
+# }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
-}
+#   owners = ["099720109477"] # Canonical
+# }
 
 # resource "aws_instance" "web" {
 #   ami           = "${data.aws_ami.ubuntu.id}"
