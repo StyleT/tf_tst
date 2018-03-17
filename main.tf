dbcbf1b4ea8bd5419c7ba4abed57ce4e59bd90a1
Vladlen Fedosov <vlad.fedosov@gmail.com>
2018-03-17 21:30:31 +0200

    tttt

--- main.tf
+++ main.tf
@@ -8,21 +8,21 @@ provider "aws" {
   region     = "us-west-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
-}
+#   owners = ["099720109477"] # Canonical
+# }
 
 # resource "aws_instance" "web" {
 #   ami           = "${data.aws_ami.ubuntu.id}"
